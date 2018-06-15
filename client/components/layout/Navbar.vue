29db49dbfc7f714ff9e009649c3fca1f48963203
HarisSistek <ha.sistek@gmail.com>
2018-06-15 13:05:55 +0200

    Navbar changed to 0.7 version

--- client/components/layout/Navbar.vue
+++ client/components/layout/Navbar.vue
@@ -1,25 +1,17 @@
 <template>
-  <section class="hero is-bold app-navbar animated" :class="{ slideInDown: show, slideOutDown: !show }">
+  <section class="hero is-white app-navbar">
     <div class="hero-head">
-      <nav class="nav">
-        <div class="nav-left">
-          <a class="nav-item is-hidden-tablet" @click="toggleSidebar({opened: !sidebar.opened})">
-            <i class="fa fa-bars" aria-hidden="true" v-show="!sidebar.hidden"></i>
+      <nav class="navbar" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+          <a class="navbar-item" href="https://bulma.io">
+            <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28">
           </a>
-        </div>
-        <div class="nav-center">
-          <a class="nav-item hero-brand" href="/">
-            <img src="~assets/logo.svg" :alt="pkginfo.description">
-            <!--<tooltip :label="'v' + pkginfo.version" placement="right" type="success" size="small" :no-animate="true" :always="true" :rounded="true">-->
-              <!--<div class="is-hidden-mobile">-->
-                <!--<span class="vue">Vue</span><strong class="admin">Admin</strong>-->
-              <!--</div>-->
-            <!--</tooltip>-->
+
+          <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
           </a>
-        </div>
-        <div class="nav-right is-flex">
-          <router-link v-if="!$auth.check()" to="/login" class="nav-item">Login</router-link>
-          <a v-if="$auth.check()" @click="logout" class="nav-item">Logout</a>
         </div>
       </nav>
     </div>
